753de8ba31273c4e98692490e8451ebba9699746
Tim <thung1@binghamton.edu>
2015-11-23 22:38:05 -0500

    Update README and remove unnecessary lines

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Solutions to problems I have encountered at programming competitions.
 #### - [Microsoft Coding Competition 2014](/MS Coding Competition 2014/)
 #### - [Microsoft Coding Competition 2015](/MS Coding Competition 2015/)
 #### - [Hacker Rank World Cup 2015](/Hacker Rank World Cup 2015/)
+#### - [CodeCon 2015](/codecon-2015/)
--- codecon-2015/codeCon1.java
+++ codecon-2015/codeCon1.java
@@ -1,21 +1,11 @@
 import java.util.Scanner;
 import java.lang.StringBuffer;
 
-//Your submission should *ONLY* use the following class name
 public class codeCon1
 {
-    public static void main(String[] args)
-    {
-
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
-        //int num = in.nextInt();
-        //num = 26;
-        //System.out.println(num);
-
-        for(int i = 1; i < 200; i++) convert(i);
-
-
+        convert(in.nextInt());
     }
 
     public static void convert(int num) {
@@ -23,27 +13,19 @@ public class codeCon1
         System.out.print(num + ":");
         
         while(num > 0) {
-
-            int val = num%26;
+            int val = num % 26;
 
             if(val == 0) {
                 val = 90;
-                num = num/ 26 - 1;
+                num = num / 26 - 1;
             }
             else {
                 val += 64;
-                num = num/26;
+                num = num / 26;
             }
-                
-            //if(val == 64) val = 90;
-            
             char cur = (char) val;
             sb.append(cur);
-
-            //if(num % 26 == 25) num -= 26;
-            //else num = num/26;
         }
         System.out.println(sb.reverse().toString().toUpperCase());
     }
-
 }
